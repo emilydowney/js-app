5b63d62cf031a7ae7fcff8a70e77a5a70b496c9c
emilydowney <80785791+emilydowney@users.noreply.github.com>
2021-04-04 16:30:09 -0500

    Addition of pokemonList

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-alert('Hello world!');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  { name : 'charmander',
+    height : 0.6,
+    type : 'fire',
+    abilities : ['blaze', 'solar-power']
+  },
+  { name : 'squirtle',
+    height : 0.5,
+    type : 'water',
+    abilities : ['rain-dish', 'torrent']
+  },
+  { name : 'bulbasaur',
+    height : 0.7,
+    type : ['grass', 'poison'],
+    abilities : ['chlorophyll', 'overgrow']
+  },
+  { name : 'pidgey',
+    height : 0.3,
+    type : ['flying', 'normal'],
+    abilities : ['keen-eye', 'tangled-feet', 'big-pecks']
+  },
+  { name : 'jigglypuff',
+    height : 0.5,
+    type : ['fairy', 'normal'],
+    abilities : ['cute-charm', 'friend-guard']
+  }
+];
